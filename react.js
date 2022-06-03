f05a6ef1b10bf035a2399fdee94d86a733eea9fd
Fedor Zajac <fedor.zajac@sk.ibm.com>
2022-06-03 14:13:53 +0200

    add react stuff

---
+++ react.js
@@ -0,0 +1,35 @@
+const { useState, 
+    // useEffect 
+} = React;
+
+const App = () => {
+
+    const [name, setName] = useState('');
+    const [catFact, setCatFact] = useState();
+
+    const inputHandler = e => {
+        setName(e.target.value);
+    }
+
+    const buttonHandler = () => {
+        fetch('https://catfact.ninja/fact')
+            .then(response => response.json())
+            .then(data => 
+                {
+                    console.log(data);
+                    setCatFact(data);
+                });
+    }
+
+    return (
+        <React.Fragment>
+            <h1>🦄</h1>
+            <button onClick={buttonHandler} >Show me Cat random fact</button><br />
+            <span>{catFact?.fact}</span>
+            <span>{catFact?.length}</span>
+        </React.Fragment>
+    );
+}
+
+let domContainer = document.querySelector('#app');
+ReactDOM.render(<App />, domContainer);
--- readme.md
+++ readme.md
@@ -1,3 +1,16 @@
 git add index.html
 git commit -m 'we add header and body'
 git push
+
+
+download template from https://html5up.net/
+extract the .zip somewhere and copy/paste its content into your project directory
+
+- + fedorzajac.github.io/
+  | - .git
+  | - index.html
+  + + assets
+    | - ....
+  ...
+
+ 
